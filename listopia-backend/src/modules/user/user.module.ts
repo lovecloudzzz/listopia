2fe5802ef0c3706856d38cae8c668266a30a8e0d
Дмитрий Моисеенко <lovecloudez@gmail.com>
2024-07-06 21:33:49 +0300

    :man: base User module without controller

--- listopia-backend/src/modules/user/user.module.ts
+++ listopia-backend/src/modules/user/user.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { UserResolver } from './user.resolver';
-import { AuthModule } from '../auth/auth.module';
-import { AuthResolver } from '../auth/auth.resolver';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserResolver, AuthResolver],
-  imports: [AuthModule],
+  providers: [UserService],
+  imports: [],
 })
 export class UserModule {}
